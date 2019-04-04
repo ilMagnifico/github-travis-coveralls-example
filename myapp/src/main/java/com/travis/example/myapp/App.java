00527a9952e7be335caded98ade8e240454023b2
ilMagnifico <lorenzo@mymail.ie>
2019-04-04 17:38:07 +0200

    first commit

---
+++ myapp/src/main/java/com/travis/example/myapp/App.java
@@ -0,0 +1,18 @@
+package com.travis.example.myapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public String sayHello(String name) {
+		if (name == null)
+			return "Hello";
+		else
+			return "Hello " + name;
+	}
+
+	public String sayHello() {
+		return sayHello(null);
+	}
+}
---
+++ myapp/src/test/java/com/travis/example/myapp/AppTest.java
@@ -0,0 +1,29 @@
+package com.travis.example.myapp;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+public class AppTest {
+	private App app;
+
+	@Before
+	public void setup() {
+		app = new App();
+	}
+
+	@Test
+	public void testSayHello() {
+		assertEquals("Hello", app.sayHello());
+	}
+
+	@Test
+	public void testSayHelloWithName() {
+		assertEquals("Hello World", app.sayHello("World"));
+	}
+}
